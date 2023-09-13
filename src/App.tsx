5f00d1ff8b6eff510cd05ff32ed9d92a5e465f7a
jordan <jordan.lee@formssihk.com>
2023-09-13 10:55:58 +0800

    first commit.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import {
+  Admin,
+  Resource
+} from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { UserList } from "./users";
+
+export const App = () => <Admin dataProvider={dataProvider}>
+  <Resource name="users" list={UserList} />
+</Admin>;
